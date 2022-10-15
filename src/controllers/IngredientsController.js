2227b55592f622a401994b7ae695623d15fa52c2
Monica <contatomonicavaz@gmail.com>
2022-10-15 11:39:41 -0300

    feat: add ingredients controller

--- src/controllers/IngredientsController.js
+++ src/controllers/IngredientsController.js
@@ -1,3 +1,26 @@
-class IngredientsController {}
+const {
+  IngredientsRepository,
+} = require('../repositories/ingredients/IngredientsRepository');
+const {
+  IngredientCreateService,
+} = require('../services/ingredients/IngredientCreateService');
+
+module.exports = { IngredientsController };
+class IngredientsController {
+  async create(req, res) {
+    const { name } = JSON.parse(req.body.data);
+    const imageFilename = req.file.filename;
+
+    const repository = new IngredientsRepository();
+    const ingredientCreateService = new IngredientCreateService(repository);
+
+    await ingredientCreateService.execute({
+      name,
+      image: imageFilename,
+    });
+
+    return res.status(201).json();
+  }
+}
 
 module.exports = IngredientsController;
