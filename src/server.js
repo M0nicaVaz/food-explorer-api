5da5314280061a4d8e48bf1230761b87543bf88d
Monica <contatomonicavaz@gmail.com>
2022-10-15 11:40:27 -0300

    feat: add express static route

--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ const AppError = require('./utils/AppError');
 const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
+const uploadConfig = require('./configs/upload');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
